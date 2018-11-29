7cd178597d4373809379ca92ac172d61192a3015
Jack Lowrie <jack@4thwall.io>
2018-11-29 15:40:10 -0600

    Startup (plugin header and license added)

---
+++ time-ago.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name:  General Admission for Woocommerce
+Plugin URI:   https://4thwall.io
+Description:  WordPress Plugin that extends Woocommerce to facilitate simple general admission events. Ideal for small nonprofits such as contemporary music ensembles and storefront theatres, or for speaking events.
+Version:      1.0
+Author:       4th Wall Websites
+Author URI:   https://4thwall.io
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+
+General Admission is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+General Admission is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with General Admission. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
