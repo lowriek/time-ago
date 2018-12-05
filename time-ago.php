027e2172cb5356a28851565b5fddeb7bdf264c7e
Jack Lowrie <jack@4thwall.io>
2018-12-05 15:56:34 -0600

    + Register settings + add shell for validating options + add settings fields shell + fix indentation

--- time-ago.php
+++ time-ago.php
@@ -59,40 +59,70 @@ $Time_Ago = new Time_Ago(); // run it!
 class Time_Ago_Settings {
   public function __construct(){
     // add Time Ago Settings submenu
-    add_action('admin_menu', array( $this , 'time_ago_settings_menu'));
+    add_action( 'admin_menu', array( $this , 'time_ago_settings_menu' ) );
+    add_action( 'admin_init', array( $this , 'time_ago_register_settings' ) );
+
   }
-  /*Settings functions*/
-      public function time_ago_settings_menu() {
-        add_submenu_page(
-            'options-general.php',
-            'Time Ago Options',
-            'Time Ago',
-            'manage_options',
-            'time_ago',
-            array($this, 'time_ago_settings_page_html')
-        );
-      }
 
 
-      public function time_ago_settings_page_html() {
-      // check user capabilities
-      if ( ! current_user_can('manage_options') ) return 'insufficient privileges';
-      ?>
-      <div class="wrap">
-          <h1><?= esc_html(get_admin_page_title()); ?></h1>
-          <form action="options.php" method="post">
-              <?php
-              // output security fields for the registered setting "wporg_options"
-              settings_fields('time_ago_options');
-              // output setting sections and their fields
-              // (sections are registered for "wporg", each field is registered to a specific section)
-              do_settings_sections('time_ago');
-              // output save settings button
-              submit_button('Save Settings');
-              ?>
-          </form>
-      </div>
-      <?php
+
+  public function time_ago_register_settings() {
+    register_setting(
+      'time_ago_options',
+      'time_ago_options',
+      array( $this , 'time_ago_validate_options' )
+    );
+
+    add_settings_field(
+      'time_ago_default',
+      'Time Ago Original Display',
+      array( $this , 'time_ago_settings_callback' ),
+      'time_ago',
+      'default',
+      array('id' => 'time_ago_default', 'label' => 'Choose the default Time Ago format')
+    );
+  }
+
+  public function time_ago_settings_callback($args) {
+    echo "checkboxes go here";
+  }
+
+  public function time_ago_validate_options($input) {
+    return $input;
+  }
+
+  // this makes the submenu page in the dashboard
+  public function time_ago_settings_menu() {
+    add_submenu_page(
+        'options-general.php',
+        'Time Ago Options',
+        'Time Ago',
+        'manage_options',
+        'time_ago',
+        array($this, 'time_ago_settings_page_html')
+    );
+  }
+
+  // the form on the submenu page in the dashboard (which time_ago_settings_menu created)
+  public function time_ago_settings_page_html() {
+    // check user capabilities
+    if ( ! current_user_can('manage_options') ) return 'insufficient privileges';
+    ?>
+    <div class="wrap">
+        <h1><?= esc_html(get_admin_page_title()); ?></h1>
+        <form action="options.php" method="post">
+            <?php
+            // output security fields for the registered setting "wporg_options"
+            settings_fields('time_ago_options');
+            // output setting sections and their fields
+            // (sections are registered for "wporg", each field is registered to a specific section)
+            do_settings_sections('time_ago');
+            // output save settings button
+            submit_button('Save Settings');
+            ?>
+        </form>
+    </div>
+    <?php
   }
 }
 $Time_Ago_settings = new Time_Ago_Settings();
